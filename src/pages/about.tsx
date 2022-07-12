5254e82c2c217f94704d4e88d237ca1d87807832
Faris <farisraihanhadibrata@gmail.com>
2022-07-12 19:33:19 +0700

    About

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,29 @@
+import { useEuiFontSize } from "@elastic/eui";
+
 function AboutPage() {
+  const titleSize = useEuiFontSize("xl");
   return (
-    <div>AboutPage</div>
-  )
+    <>
+    <div
+      className="eui-textCenter"
+      style={{
+        fontSize: titleSize.fontSize,
+      }}
+    >
+      Drag And drop Component React
+    </div>
+    <div style={{
+        marginTop : "20px",
+    }}>
+        Following are Example to create Drag and drop react component, along with example how to example fetching the data from API separately, feel free to try it out.<br/>
+        Made using React, React-Query, Eui, Elastic-UI, and many other libraries. <br/>
+        <a href="https://github.com/Farishadibrata/dragdrop" target={"_blank"}>
+            Github
+        </a>
+    </div>
+    </>
+
+  );
 }
 
-export default AboutPage
+export default AboutPage;
